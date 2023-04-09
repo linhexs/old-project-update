879741268552b5977ee5a325c3b3b0100d66db93
linhexs <838311829@qq.com>
2023-04-09 16:47:21 +0800

    feat: 首页登录也移植配置

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('this is webpack project')
+import './css/public.css'
+import './css/index.css'
+import './css/login.css'
+
+import 'jquery'
+import './js/public'
+import './js/nav'
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    bundle: "./src/index.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "./dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          // 如果一个模块源码大小小于 maxSize，那么模块会被作为一个 Base64 编码的字符串注入到包中， 否则模块文件会被生成到输出的目标目录中。
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        // 输出目录
+        generator: {
+          filename: "images/[name].[hash:6][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "login.html",
+      template: "./src/login.html",
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 };
